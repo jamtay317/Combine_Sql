8de30d2526cf9845ea3f4909b7ad8e429e4f3ed5
James Tays <james@jaila-files.com>
2018-06-04 16:54:23 -0400

    Add project files.

---
+++ Combine_Sql/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combine_Sql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
